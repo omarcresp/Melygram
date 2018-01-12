6f9770a7fdf3fe5a8591e415967e1864ba0ff63f
Omar Crespo <crespomerchano@gmail.com>
2018-01-12 12:04:51 -0400

    Creando vistas con pug

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
+app.use(express.static('assets'))
+app.use(express.static('node_modules/jquery/dist'))
+app.use(express.static('node_modules/materialize-css/dist/js'))
+
 app.get('/', function (req, res) {
-  res.send('Hola Mundo!')
+  res.render('index')
 })
 
 app.listen(3000, function (err) {
